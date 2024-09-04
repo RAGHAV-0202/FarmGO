06e715035a6ee2dc3e55361f204f27e4ac6ab817
Raghav Kapoor <raghavkapoor16947@gmail.com>
2024-09-04 23:58:25 +0530

    commit

--- Frontend/src/Pages/login_page.js
+++ Frontend/src/Pages/login_page.js
@@ -24,7 +24,7 @@ function LoginPageComponent({setStep}){
        try {
         alert("please wait, process may take 4-5 seconds")
         console.log(email , password)
-            const response = await axios.post("http://localhost:8000/api/auth/login", { login : email, password }, { withCredentials: true })
+            const response = await axios.post("https://farmgo-production.up.railway.app/api/auth/login", { login : email, password }, { withCredentials: true })
             console.log(response.data.message)
 
             emailContainer.current.value = ""
